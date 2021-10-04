1ce3e1b66d4d644a92d64485a3d033ab2082164b
nolanzzz <nolanwork131@gmail.com>
2021-10-03 20:51:55 -0400

    add epochs

--- configs/mta/faster_rcnn_r50_mta.py
+++ configs/mta/faster_rcnn_r50_mta.py
@@ -162,5 +162,9 @@ log_level = 'INFO'
 workflow = [('train', 1)]
 checkpoint_config = dict(interval=1)
 
+# optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+
+# runtime settings
+total_epochs = 3
