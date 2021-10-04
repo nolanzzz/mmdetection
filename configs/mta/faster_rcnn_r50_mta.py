2799688f191a6fe0bcaee064b184c47add23ec08
nolanzzz <nolanwork131@gmail.com>
2021-10-03 20:50:03 -0400

    add optimizer to config

--- configs/mta/faster_rcnn_r50_mta.py
+++ configs/mta/faster_rcnn_r50_mta.py
@@ -161,3 +161,6 @@ evaluation = dict(interval=1, metric='bbox')
 log_level = 'INFO'
 workflow = [('train', 1)]
 checkpoint_config = dict(interval=1)
+
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
